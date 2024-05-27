0869668cc6ab361e5ea610a8d77af2cd6018ef00
dzx <royeedai@gmail.com>
2024-05-27 17:13:50 +0800

    更改接口路径

--- app/adminapi/middleware/JWT.php
+++ app/adminapi/middleware/JWT.php
@@ -2,7 +2,7 @@
 
 declare (strict_types = 1);
 
-namespace app\admin\middleware;
+namespace app\adminapi\middleware;
 
 use app\service\api\admin\authority\AccessTokenService;
 use app\service\api\admin\authority\AdminUserService;
