7ca4940bc7037efcfa6e7ee7f789fd00d6b68c62
hxl <3721@163.com>
2024-05-27 14:12:52 +0800

    #路由调整

---
+++ app/index/route/cart.php
@@ -0,0 +1,21 @@
+<?php
+use think\facade\Route;
+
+// 购物车
+Route::group('cart', function () {
+    // 购物车
+    Route::group('cart', function () {
+        // 购物车列表
+        Route::get('list', 'cart.cart/list');
+        // 获取购物车商品数量
+        Route::get('get_count', 'cart.cart/getCount');
+        // 更新购物车商品选择状态
+        Route::post('update_check', 'cart.cart/updateCheck');
+        // 更新购物车商品数量
+        Route::post('update_item', 'cart.cart/updateItem');
+        // 删除购物车商品
+        Route::post('remove_item', 'cart.cart/removeItem');
+        // 清空购物车
+        Route::post('clear', 'cart.cart/clear');
+    });
+});
---
+++ app/index/route/order.php
@@ -0,0 +1,37 @@
+<?php
+use think\facade\Route;
+
+// 订单
+Route::group('order', function () {
+
+    // 订单结算
+    Route::group('check', function () {
+        // 结算
+        Route::post('index', 'order.check/index');
+        // 订单
+        Route::post('update', 'order.check/update');
+        // 订单
+        Route::post('update_coupon', 'order.check/updateCoupon');
+        // 订单提交
+        Route::post('submit', 'order.check/submit');
+        // 获得上次订单发票信息
+        Route::get('get_invoice', 'order.check/getInvoice');
+
+    });
+
+    // 订单支付
+    Route::group('pay', function () {
+        // 支付页信息
+        Route::get('index', 'order.pay/index');
+        // 订单状态
+        Route::get('check_status', 'order.pay/checkStatus');
+        // 支付
+        Route::post('create', 'order.pay/create');
+        // 回调
+        Route::post('notify', 'order.pay/notify');
+        // 回调
+        Route::post('notify', 'order.pay/notify');
+        // 回调
+        Route::post('refund_notify', 'order.pay/refundNotify');
+    });
+});
