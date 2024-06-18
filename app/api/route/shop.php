bf173c516c5d91d856d3747218774a684df11867
dzx <royeedai@gmail.com>
2024-06-18 10:40:29 +0800

    店铺详情

--- app/api/route/shop.php
+++ app/api/route/shop.php
@@ -8,7 +8,8 @@ Route::group('shop', function () {
     Route::group('shop', function () {
         // 购物车列表
         Route::get('decorate', 'decorate');
-
+        // 详情
+        Route::get('detail', 'detail');
     });
 })->prefix("shop.shop/")->middleware([
 
