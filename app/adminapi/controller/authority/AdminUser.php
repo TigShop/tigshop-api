9610d298ce1793e84178e8a638541a37da8ec8b4
luohonen <luohonggen@tigshop.com>
2024-06-19 16:50:24 +0800

    验证码验证

--- app/adminapi/controller/authority/AdminUser.php
+++ app/adminapi/controller/authority/AdminUser.php
@@ -19,6 +19,7 @@ use app\validate\authority\AdminUserValidate;
 use exceptions\ApiException;
 use think\App;
 use think\exception\ValidateException;
+use think\facade\Cache;
 use think\facade\Db;
 use think\Response;
 use utils\Format;
@@ -259,6 +260,7 @@ class AdminUser extends AdminBaseController
         ], 'post');
         $result = app(SmsService::class)->checkCode($data["mobile"], $data["code"]);
         if ($result === true) {
+            Cache::delete('loginmobileCode:' . $data["mobile"]);
             $this->success(/** LANG */'验证成功');
         }else{
             $this->error(/** LANG */'验证码错误');
