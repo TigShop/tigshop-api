a0a146bf4eac0cb3e9e238608658b8e846b3dafd
luohonen <luohonggen@tigshop.com>
2024-06-19 15:58:24 +0800

    手机号码增加开发形式

--- app/adminapi/controller/authority/AdminUser.php
+++ app/adminapi/controller/authority/AdminUser.php
@@ -95,8 +95,7 @@ class AdminUser extends AdminBaseController
     {
         $id = input('id/d', 0);
         $item = $this->adminUserService->getDetail($id);
-        $item['open_mobile'] = $item['mobile'];
-        $item['mobile'] = Format::dimMobile($item->mobile);
+        $item['encipher_mobile'] = Format::dimMobile($item->mobile);
 
         return $this->success([
             'item' => $item,
